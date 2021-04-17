eb52473b03d6662a3a331d48d141de2ded493b3f
Zero-i00 <mrshelkinpro228@gmail.com>
2021-04-17 21:06:36 +0500

    commit_with_converter

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QApplication, QMainWindow
+import sys
+
+
 from forex_python.converter import CurrencyRates
 
 c = CurrencyRates()
@@ -6,4 +11,14 @@ def USD(c):
     value = c.get_rate('USD', 'RUB')
     return value
 print(USD(c))
+
+def Convent(c):
+    return c.get_rate(input_currency, output_currency)
+
+input_currency = input('From the currency:').upper()
+amount = int(input('The Amount:'))
+output_currency = input('To the currency:').upper()
+will_amount = Convent(c)
+
+print(round(will_amount * amount, 2))
 
